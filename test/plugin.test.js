f44e4b117dee4e7e30507f1da2ebb62d1f5f6aa6
Hero van Commenée <hero@streamone.nl>
2016-06-15 13:13:28 +0200

    Add dutch language file

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -4,16 +4,9 @@ import QUnit from 'qunit';
 import sinon from 'sinon';
 import videojs from 'video.js';
 
-import plugin from '../src/plugin';
+// import plugin from '../src/plugin';
 
-const Player = videojs.getComponent('Player');
-
-QUnit.test('the environment is sane', function(assert) {
-  assert.strictEqual(typeof Array.isArray, 'function', 'es5 exists');
-  assert.strictEqual(typeof sinon, 'object', 'sinon exists');
-  assert.strictEqual(typeof videojs, 'function', 'videojs exists');
-  assert.strictEqual(typeof plugin, 'function', 'plugin is a function');
-});
+// const Player = videojs.getComponent('Player');
 
 QUnit.module('videojs-settings-menu', {
 
@@ -35,24 +28,4 @@ QUnit.module('videojs-settings-menu', {
     this.player.dispose();
     this.clock.restore();
   }
-});
-
-QUnit.test('registers itself with video.js', function(assert) {
-  assert.expect(2);
-
-  assert.strictEqual(
-    Player.prototype.settingsMenu,
-    plugin,
-    'videojs-settings-menu plugin was registered'
-  );
-
-  this.player.settingsMenu();
-
-  // Tick the clock forward enough to trigger the player to be "ready".
-  this.clock.tick(1);
-
-  assert.ok(
-    this.player.hasClass('vjs-settings-menu'),
-    'the plugin adds a class to the player'
-  );
 });
